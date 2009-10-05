85315db88877ea0cc2fe9352b07d085478c5bbcc
csommer <csommer@40e545cb-b269-0410-8983-4113551e4d47>
2009-10-05 13:57:20 +0000

    SVN keyword substitution for Verson/Revision Number
    git-svn-id: https://gorgonzola.iwr.uni-heidelberg.de/svn/ILASTIK/trunk/src@31 40e545cb-b269-0410-8983-4113551e4d47

--- gui/ilastikMain.py
+++ gui/ilastikMain.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 import sys
+sys.path.append("..")
 import pdb
 from PyQt4 import QtCore, QtGui
-sys.path.append("..")
 from core import version
 
 class MainWindow(QtGui.QMainWindow):
@@ -10,10 +10,10 @@ class MainWindow(QtGui.QMainWindow):
         QtGui.QMainWindow.__init__(self)
         self.setGeometry(50,50,768,512)
         self.iconPath = '../../icons/32x32/'
-        self.setWindowTitle("Ilastik 0.0." + version.getIlastikVersion())
+        self.setWindowTitle("Ilastik rev: " + version.getIlastikVersion())
 
 if __name__ == "__main__":
     app = QtGui.QApplication(sys.argv)
-    mainwindow = MainWindow()
+    mainwindow = MainWindow()  
     mainwindow.show()
     sys.exit(app.exec_())
