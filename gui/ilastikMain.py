87354e1bead55ae4d06e9fc39c2bf05ec5407bb9
skasseme <skasseme@40e545cb-b269-0410-8983-4113551e4d47>
2009-10-13 13:14:03 +0000

    use setter-method...
    git-svn-id: https://gorgonzola.iwr.uni-heidelberg.de/svn/ILASTIK/trunk/src@57 40e545cb-b269-0410-8983-4113551e4d47

--- gui/ilastikMain.py
+++ gui/ilastikMain.py
@@ -127,7 +127,7 @@ class ProjectDlg(QtGui.QDialog):
         for k in range(0, rowCount):
             fileName = self.tableWidget.itemAt(k, 0).text()
             dataItemList.append(dataMgr.DataItemImage(fileName))     
-        self.parent.project.dataList = dataItemList        
+        self.parent.project.setDataList(dataItemList)        
         self.close()
         
     
