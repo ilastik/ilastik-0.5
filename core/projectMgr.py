00c67475c849b3d310d3c28435d979e3142de93b
csommer <csommer@40e545cb-b269-0410-8983-4113551e4d47>
2010-02-06 17:21:32 +0000

    save of project now change to hdf5
    still "some features" missing
    also load is missing
    
    git-svn-id: https://gorgonzola.iwr.uni-heidelberg.de/svn/ILASTIK/trunk/src@235 40e545cb-b269-0410-8983-4113551e4d47

--- core/projectMgr.py
+++ core/projectMgr.py
@@ -29,18 +29,42 @@ class Project(object):
         projectG.create_dataset('Labeler', data=self.labeler)
         projectG.create_dataset('Description', data=self.description)
         projectG.create_dataset('LabelNames', data=self.labelNames)
-        projectG.create_dataset('LabelColors', data=[int(k.rgb()) for k in self.labelColors.values()])
+        projectG.create_dataset('LabelColors', data=[int(k.rgba()) for k in self.labelColors.values()])
         
         # get number of images
         n = len(self.dataMgr)
         
-        # create Subgroups
+        # save raw data and labels
         for k in range(n):
+            # create group for dataItem
             dk = dataSetG.create_group('dataItem%02d' % k)
+            dk.attrs["fileName"] = str(self.dataMgr[k].fileName)
+            # save raw data
             dataIt = dk.create_dataset('rawData', data=self.dataMgr[k].data)
             dataIt.attrs["dataKind"] = self.dataMgr[k].dataKind
-            dk.attrs["fileName"] = str(self.dataMgr[k].fileName)
+            # save raw labels
             labelIt = dk.create_dataset('labels', data=self.dataMgr[k].labels)
+        
+            # save features if available
+            if self.dataMgr.dataFeatures:
+                feaureG = dk.create_group('features')
+                for fCnt, (feature, featureName, channelIndex) in irange(self.dataMgr.dataFeatures[k]):
+                    featureIT = feaureG.create_dataset('feature%03d' % fCnt,data=feature)
+                    featureIT.attrs['featureName'] = featureName
+                    featureIT.attrs['channelIndex'] = channelIndex
+
+            # save prediction if available
+            if self.dataMgr.prediction[0] is not None:
+                prediction = self.dataMgr.prediction[k]
+                predictionIt = dk.create_dataset('prediction',data=prediction.reshape(self.dataMgr[k].shape[0:2] + (-1,)))
+                # predictionIt.attrs['classifier'] = 'classifierName'
+            
+            # save segmentation if available
+            if self.dataMgr.segmentation[0] is not None:
+                segmentation = self.dataMgr.segmentation[k]
+                segmentationIt = dk.create_dataset('segmentation',data=segmentation)
+                # segmentationIt.attrs['segmentationOperation'] = 'segmentationOperation'
+
         dataSetG.attrs["dataKind"] = self.dataMgr[0].dataKind
         
         # Save to hdf5 file
