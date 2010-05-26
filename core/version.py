725a71c656173c331089e602af5b420420563360
csommer <csommer@40e545cb-b269-0410-8983-4113551e4d47>
2010-05-26 13:03:52 +0000

    version file
    git-svn-id: https://gorgonzola.iwr.uni-heidelberg.de/svn/ILASTIK/trunk/src@426 40e545cb-b269-0410-8983-4113551e4d47

--- core/version.py
+++ core/version.py
@@ -29,5 +29,5 @@
 
 def getIlastikVersion():
     version = "$Revision$"  #SVN keyword, get replaced
-    version = version.split(" ")[1]   
+    version = version.split(" ")[1]    
     return version.strip()      
