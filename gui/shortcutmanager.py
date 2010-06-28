8318992965e127e16479836d02bb607f47cfd05f
tkroeger <tkroeger@40e545cb-b269-0410-8983-4113551e4d47>
2010-06-28 08:30:50 +0000

    add file
    git-svn-id: https://gorgonzola.iwr.uni-heidelberg.de/svn/ILASTIK/trunk/src@525 40e545cb-b269-0410-8983-4113551e4d47

---
+++ gui/shortcutmanager.py
@@ -0,0 +1,27 @@
+from PyQt4 import QtGui
+import sip
+
+class ShortcutManager():
+    def __init__(self):
+        self.shortcuts = dict()
+        
+    def register(self, shortcut, description):
+        self.shortcuts[shortcut.key().toString()] = description
+        
+    def showDialog(self):
+        dlg = ShortcutManagerDlg(self)
+
+class ShortcutManagerDlg(QtGui.QDialog):
+    def __init__(self, s):
+        QtGui.QDialog.__init__(self)
+        l = QtGui.QGridLayout(self)
+        
+        for i, sc in enumerate(s.shortcuts):
+            desc = s.shortcuts[sc]
+            
+            l.addWidget(QtGui.QLabel(str(sc)), i,0)
+            l.addWidget(QtGui.QLabel(desc), i,1)
+        self.setLayout(l)
+        self.exec_()
+
+shortcutManager = ShortcutManager()
