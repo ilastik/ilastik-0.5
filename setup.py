5103b2fc6fa4d1f1e879dee29931f1225a4809a6
Your full name <christoph.sommer@iwr.uni-heidelberg.de>
2010-07-21 16:16:11 +0200

    added icons to package_data in setupy.py

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
 from distutils.core import setup
 
+import os
+import sys
+iconFileList = []
+rootdir = 'ilastik/gui/icons/'
+
+for root, subFolders, files in os.walk(rootdir):
+    for file in files:
+        iconFileList.append(os.path.join(root[12:],file))
+
 setup(name = 'ilastik',
       version = '0.5',
       description = 'Interactive Learning and Segmentation Tool Kit',
@@ -10,5 +19,5 @@ setup(name = 'ilastik',
       keywords = ['segmentation', 'numpy', 'ndarray', 'image', 'classification', 'PyQt4'],
       packages = ['ilastik.core', 'ilastik.gui', 'ilastik.core.features', 'ilastik.core.classifiers'],
       py_modules = ['ilastik/ilastikMain'],
-      package_data ={'ilastik.gui':['ilastik/gui/icons/*']},
+      package_data = {'ilastik.gui' : iconFileList},
       long_description = ''' ''')
