a0ca97149f8cab761ee2f429b677ca0a78275ce6
cstraehl <christoph.straehle@iwr.uni-heidelberg.de>
2010-08-30 13:57:05 +0200

    support picking: 	double click on any isosurface point in the mayavi 3d view 	to center the slice views to that point initialize the camera of the 3d mayavi widget correctly

--- ilastik/core/volume.py
+++ ilastik/core/volume.py
@@ -28,7 +28,6 @@ class DataAccessor():
         """
         
         if issubclass(data.__class__,  DataAccessor):
-            self = data
             self.data = data.data
             self.rgb = data.rgb
             self.shape = data.shape
