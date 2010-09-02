b3497ea7d3329832a921555bc50ec8fa4b240d52
csommer <christoph.sommer@iwr.uni-heidelberg.de>
2010-09-02 10:16:10 +0200

    added missing file

---
+++ ilastik/gui/ribbons/ilastikTabBase.py
@@ -0,0 +1,14 @@
+
+class IlastikTabBase(object):
+    name = "Ribbon Base Class for Tab Pages" 
+    description = "Virtual base class" 
+    author = "HCI, University of Heidelberg"
+    homepage = "http://hci.iwr.uni-heidelberg.de"
+
+    def __init__(self, parent=None):
+        self.parent = parent
+    
+    def on_activation(self):
+        print "Tab changed: on_activation() not implementated by this tab"
+        
+
