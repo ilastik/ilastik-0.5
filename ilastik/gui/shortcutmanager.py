c6db6157446ab299e9fee1b8628835db3073e1b4
akreshuk <Anna.Kreshuk@iwr.uni-heidelberg.de>
2010-09-10 14:23:30 +0200

    merge again

---
+++ ilastik/gui/shortcutmanager.py
@@ -0,0 +1,32 @@
+from PyQt4 import QtGui
+
+class shortcutManager():
+    def __init__(self):
+        self.shortcuts = dict()
+        
+    def register(self, shortcut, description):
+        self.shortcuts[shortcut.key().toString()] = description
+        
+    def showDialog(self):
+        dlg = shortcutManagerDlg(self)
+
+class shortcutManagerDlg(QtGui.QDialog):
+    def __init__(self, s):
+        QtGui.QDialog.__init__(self)
+        if len(s.shortcuts)>0:
+            l = QtGui.QGridLayout(self)
+            
+            for i, sc in enumerate(s.shortcuts):
+                desc = s.shortcuts[sc]
+                
+                l.addWidget(QtGui.QLabel(str(sc)), i,0)
+                l.addWidget(QtGui.QLabel(desc), i,1)
+            self.setLayout(l)
+            self.exec_()
+        else:
+            l = QtGui.QVBoxLayout()
+            l.addWidget(QtGui.QLabel("Load the data by pressing the \"New\" button in the project dialog"))
+            self.setLayout(l)
+            self.exec_()
+            
+shortcutManager = shortcutManager()
