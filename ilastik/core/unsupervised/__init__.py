ed2604c6f8a1eb33f17b0dde32b6106991e39d88
unknown <Michael Hanselmann@.(none)>
2010-09-29 20:00:24 +0200

    added files

---
+++ ilastik/core/unsupervised/__init__.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#    Copyright 2010 C Sommer, C Straehle, M Hanselmann, U Koethe, FA Hamprecht. All rights reserved.
+#
+#    Redistribution and use in source and binary forms, with or without modification, are
+#    permitted provided that the following conditions are met:
+#
+#       1. Redistributions of source code must retain the above copyright notice, this list of
+#          conditions and the following disclaimer.
+#
+#       2. Redistributions in binary form must reproduce the above copyright notice, this list
+#          of conditions and the following disclaimer in the documentation and/or other materials
+#          provided with the distribution.
+#
+#    THIS SOFTWARE IS PROVIDED BY THE ABOVE COPYRIGHT HOLDERS ``AS IS'' AND ANY EXPRESS OR IMPLIED
+#    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+#    FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE ABOVE COPYRIGHT HOLDERS OR
+#    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+#    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#    ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#    The views and conclusions contained in the software and documentation are those of the
+#    authors and should not be interpreted as representing official policies, either expressed
+#    or implied, of their employers.
+
+import traceback,  os,  sys,  unsupervisedBase
+
+#
+#Import other unsupervised plugins dynamically
+#
+load = False
+try:
+    test = unsupervisedClasses
+except Exception,  e:
+    load = True
+    
+
+if load:
+    pathext = os.path.dirname(__file__)
+    try:
+        for f in os.listdir(os.path.abspath(pathext)):
+            module_name, ext = os.path.splitext(f) # Handles no-extension files, etc.
+            if ext == '.py': # Important, ignore .pyc/other files.
+                module = __import__('core.unsupervised.' + module_name)
+    except Exception, e:
+        #traceback.print_exc(file=sys.stdout)
+        pass
+
+    for i, c in enumerate(unsupervisedBase.UnsupervisedBase.__subclasses__()):
+        print "loaded unsupervised decomposer ",c, ': ',  c.name
+        pass
+        
+    unsupervisedClasses = unsupervisedBase.UnsupervisedBase.__subclasses__()
+    if len(unsupervisedClasses) == 0:
+        unsupervisedClasses = [unsupervisedBase.UnsupervisedBase]
+        
