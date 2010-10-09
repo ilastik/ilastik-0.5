c84348be2ab66007889fa5ab59c890f7bd04aee0
cstraehl <christoph.straehle@iwr.uni-heidelberg.de>
2010-10-09 14:05:00 +0200

    add files

---
+++ ilastik/modules/object_picking/core/objectModuleMgr.py
@@ -0,0 +1,146 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#    Copyright 2010 C Sommer, C Straehle, U Koethe, FA Hamprecht. All rights reserved.
+#    
+#    Redistribution and use in source and binary forms, with or without modification, are
+#    permitted provided that the following conditions are met:
+#    
+#       1. Redistributions of source code must retain the above copyright notice, this list of
+#          conditions and the following disclaimer.
+#    
+#       2. Redistributions in binary form must reproduce the above copyright notice, this list
+#          of conditions and the following disclaimer in the documentation and/or other materials
+#          provided with the distribution.
+#    
+#    THIS SOFTWARE IS PROVIDED BY THE ABOVE COPYRIGHT HOLDERS ``AS IS'' AND ANY EXPRESS OR IMPLIED
+#    WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+#    FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE ABOVE COPYRIGHT HOLDERS OR
+#    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+#    CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+#    SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+#    ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#    NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+#    ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#    
+#    The views and conclusions contained in the software and documentation are those of the
+#    authors and should not be interpreted as representing official policies, either expressed
+#    or implied, of their employers.
+
+import vigra, numpy
+import traceback, os, sys
+
+from ilastik.core.volume import DataAccessor, Volume, VolumeLabels, VolumeLabelDescription
+from ilastik.core.baseModuleMgr import BaseModuleDataItemMgr, BaseModuleMgr
+from ilastik.core.overlayMgr import OverlayItem
+
+
+class ObjectOverlayItem(OverlayItem):
+    def __init__(self, objectListWidget, data, color = 0, alpha = 0.4, colorTable = None, autoAdd = False, autoVisible = False,  linkColorTable = False, autoAlphaChannel = True, min = None, max = None):
+        self.objectListWidget = objectListWidget
+        OverlayItem.__init__(self, data, color, alpha, autoAdd, autoVisible,  linkColorTable, autoAlphaChannel, min, max)
+        
+    def getColorTab(self):
+        return self.objectListWidget.getColorTab()
+
+
+class ObjectPickingItemModuleMgr(BaseModuleDataItemMgr):
+    name = "Object_Picking"
+    
+    def __init__(self, dataItemImage):
+        BaseModuleDataItemMgr.__init__(self, dataItemImage)
+        self.dataItemImage = dataItemImage
+        self.objects = None
+        self.overlays = []
+        self.selectedObjects = {}
+        self.selectionAccessor = None
+        self.inputData = None
+        
+    def onAppend(self):
+        if self.objects is None:
+            l = numpy.zeros(self.dataItemImage.shape[0:-1] + (1, ),  'uint8')
+            self.objects = VolumeLabels(l)        
+
+    def newLabels(self,  newLabels):
+        repaint = False
+        if self.inputData is not None:
+            try:
+                for nl in newLabels:
+                    indic =  list(numpy.nonzero(nl._data))
+                    indic[0] = indic[0] + nl.offsets[0]
+                    indic[1] += nl.offsets[1]
+                    indic[2] += nl.offsets[2]
+                    indic[3] += nl.offsets[3]
+                    indic[4] += nl.offsets[4]
+                    for index, ind0 in enumerate(indic[0]):
+                        selector = (indic[0][index],indic[1][index],indic[2][index],indic[3][index],indic[4][index])
+                        res = self.inputData[selector]
+                        if nl.erasing == False:
+                            self.selectedObjects[res] = res
+                        else:
+                            self.selectedObjects.pop(res, None)
+            except Exception, e:
+                print e
+                traceback.print_exc(file=sys.stdout)
+        
+        ov = self.dataItemImage.overlayMgr["Objects/Selection Result"]
+        
+        if ov is not None:
+            ov.setSelectedNumbers(self.selectedObjects.values())
+
+
+    def addLabel(self, name,number, color):
+        description = VolumeLabelDescription(name,number, color,  None)
+        description._prediction = numpy.zeros(self.dataItemImage.shape[0:-1],  'uint8')
+        self.objects.descriptions.append(description)
+            
+
+    def changedLabel(self,  label):
+        for labelIndex,  labelItem in enumerate(self.objects):
+            labelItem.name = label.name
+            labelItem.number = label.number
+            labelItem.color = label.color
+                
+    def removeLabel(self, number):
+        for j, ld in enumerate(self.objects.descriptions):
+            if ld.number == number:
+                ldnr = j
+        if ldnr != -1:
+            self.objects.descriptions.pop(ldnr)
+            for j, ld in enumerate(self.objects.descriptions):
+                if ld.number > number:
+                    ld.number -= 1
+            temp = numpy.where(self.objects._data[:,:,:,:,:] == number, 0, self.objects._data[:,:,:,:,:])
+            temp = numpy.where(temp[:,:,:,:,:] > number, temp[:,:,:,:,:] - 1, temp[:,:,:,:,:])
+            self.objects._data[:,:,:,:,:] = temp[:,:,:,:,:]
+            if self.objects._history is not None:
+                self.objects._history.removeLabel(number)
+       
+    def setInputData(self, data):
+        self.inputData = data
+
+
+
+
+
+class ObjectPickingModuleMgr(BaseModuleMgr):
+    name = "Object_Picking"
+    
+    def __init__(self, dataMgr):
+        BaseModuleMgr.__init__(self, dataMgr)
+        self.dataMgr = dataMgr
+        self.dataMgr = dataMgr
+        
+        
+        
+        
+    def onNewImage(self, dataItemImage):
+        dataItemImage.Object_Picking.onAppend()
+
+        
+
+        
+
+    
+    
+    
---
+++ ilastik/modules/object_picking/gui/objectPickingRibbon.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+import numpy, vigra
+import random
+
+from ilastik.gui.ribbons.ilastikTabBase import IlastikTabBase
+
+from PyQt4 import QtGui, QtCore
+
+from ilastik.gui.iconMgr import ilastikIcons
+
+from ilastik.gui.overlaySelectionDlg import OverlaySelectionDialog
+from ilastik.gui.overlayWidget import OverlayWidget
+from ilastik.modules.object_picking.core.objectModuleMgr import ObjectOverlayItem
+from ilastik.core.volume import DataAccessor
+import ilastik.gui.volumeeditor as ve
+from objectWidget import ObjectListWidget
+
+class ObjectsTab(IlastikTabBase, QtGui.QWidget):
+    name = 'Objects'
+    
+    def __init__(self, parent=None):
+        IlastikTabBase.__init__(self, parent)
+        QtGui.QWidget.__init__(self, parent)
+        
+        self._initContent()
+        self._initConnects()
+        
+    def on_activation(self):
+        if self.ilastik.project is None:
+            return
+        ovs = self.ilastik._activeImage.Object_Picking.overlays
+        if len(ovs) == 0:
+            raw = self.ilastik._activeImage.overlayMgr["Raw Data"]
+            if raw is not None:
+                ovs.append(raw.getRef())        
+        
+        self.ilastik.labelWidget._history.volumeEditor = self.ilastik.labelWidget
+
+        overlayWidget = OverlayWidget(self.ilastik.labelWidget, self.ilastik._activeImage.Object_Picking,  self.ilastik._activeImage.Object_Picking.overlays)
+        self.ilastik.labelWidget.setOverlayWidget(overlayWidget)
+        
+        self.objectLabels = ObjectListWidget(self.ilastik._activeImage.Object_Picking,  self.ilastik._activeImage.Object_Picking.objects,  self.ilastik.labelWidget) 
+        self.ilastik.labelWidget.setLabelWidget(self.objectLabels)
+        
+        #create ObjectsOverlay
+        ov = ObjectOverlayItem(self.objectLabels, self.ilastik._activeImage.Object_Picking.objects._data, color = 0, alpha = 1.0, autoAdd = True, autoVisible = True,  linkColorTable = True)
+        self.ilastik._activeImage.overlayMgr["Objects/Selection"] = ov
+        ov = self.ilastik._activeImage.overlayMgr["Objects/Selection"]
+        
+        self.ilastik.labelWidget.setLabelWidget(self.objectLabels)
+    
+    def on_deActivation(self):
+        if self.ilastik.project is None:
+            return
+        self.ilastik._activeImage.Object_Picking.objects._history = self.ilastik.labelWidget._history
+        
+        if self.ilastik._activeImage.Object_Picking.objects._history is not None:
+            self.ilastik.labelWidget._history = self.ilastik._activeImage.Object_Picking.objects._history
+        
+    def _initContent(self):
+        tl = QtGui.QHBoxLayout()
+        
+        self.btnChooseWeights = QtGui.QPushButton(QtGui.QIcon(ilastikIcons.Select),'Select overlay')
+        self.btnSegment = QtGui.QPushButton(QtGui.QIcon(ilastikIcons.Play),'3D')
+        
+        self.btnChooseWeights.setToolTip('Choose the edge weights for the segmentation task')
+        self.btnSegment.setToolTip('Segment the image into foreground/background')
+        
+        tl.addWidget(self.btnChooseWeights)
+        tl.addWidget(self.btnSegment)
+        tl.addStretch()
+        
+        self.setLayout(tl)
+        
+    def _initConnects(self):
+        self.connect(self.btnChooseWeights, QtCore.SIGNAL('clicked()'), self.on_btnChooseWeights_clicked)
+        self.connect(self.btnSegment, QtCore.SIGNAL('clicked()'), self.on_btnSegment_clicked)
+        
+        
+    def on_btnChooseWeights_clicked(self):
+        dlg = OverlaySelectionDialog(self.parent,  singleSelection = True)
+        answer = dlg.exec_()
+        
+        if len(answer) > 0:
+            import ilastik.core.overlays.selectionOverlay
+            if self.parent.project.dataMgr[self.parent._activeImageNumber].overlayMgr["Objects/Selection Result"] is None:
+                ov = ilastik.core.overlays.selectionOverlay.SelectionOverlay(answer[0]._data, color = long(QtGui.QColor(0,255,255).rgba()))
+                self.parent.project.dataMgr[self.parent._activeImageNumber].overlayMgr["Objects/Selection Result"] = ov
+                ov = self.parent.project.dataMgr[self.parent._activeImageNumber].overlayMgr["Objects/Selection Result"]
+            
+            ref = answer[0].getRef()
+            ref.setAlpha(0.4)
+            self.parent.labelWidget.overlayWidget.addOverlayRef(ref)
+            
+            self.parent.project.dataMgr[self.parent.project.dataMgr._activeImageNumber].Object_Picking.setInputData(answer[0]._data)
+                
+            self.parent.labelWidget.repaint()
+
+    def on_btnSegment_clicked(self):
+        pass
