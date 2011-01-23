69146ee4f1b602e7286645ba23a3a9638f2a00d2
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2011-01-23 13:07:04 +0100

    improve looks

--- ilastik/gui/view3d.py
+++ ilastik/gui/view3d.py
@@ -1,7 +1,9 @@
 from vtk import vtkRenderer, vtkConeSource, vtkPolyDataMapper, vtkActor, \
                 vtkImplicitPlaneWidget2, vtkImplicitPlaneRepresentation, \
                 vtkObject, vtkPNGReader, vtkImageActor, QVTKWidget, \
-                vtkRenderWindow
+                vtkRenderWindow, vtkOrientationMarkerWidget, vtkAxesActor, \
+                vtkTransform, vtkPolyData, vtkPoints, vtkCellArray, \
+                vtkTubeFilter
                 
 from PyQt4.QtGui import QWidget, QVBoxLayout, QHBoxLayout, QPushButton
 from PyQt4.QtCore import SIGNAL
@@ -10,6 +12,7 @@ import qimage2ndarray
 
 class SlicingPlanesWidget(vtkObject):
     def __init__(self, dataShape):
+        self.dataShape = dataShape
         self.planes = []
         self.coordinate = [0,0,0]
         for i in range(3):
@@ -19,15 +22,37 @@ class SlicingPlanesWidget(vtkObject):
             p.ScaleEnabledOff()
             p.SetOrigin(0.25,0.25,0.25)
             p.PlaceWidget([0.1,dataShape[0],0.1,dataShape[1],0.1,dataShape[2]])
-            if i==0: p.SetNormal(1,0,0)
-            elif i==1: p.SetNormal(0,1,0)
-            else: p.SetNormal(0,0,1)
-            p.GetOutlineProperty().SetColor(1,0,0)
+            if i==0:
+                p.SetNormal(1,0,0)
+                p.GetSelectedPlaneProperty().SetColor(1,0,0)
+                p.GetEdgesProperty().SetColor(1,0,0) #bug in VTK
+            elif i==1:
+                p.SetNormal(0,1,0)
+                p.GetSelectedPlaneProperty().SetColor(0,1,0)
+                p.GetEdgesProperty().SetColor(0,1,0) #bug in VTK
+            else: 
+                p.SetNormal(0,0,1)
+                p.GetSelectedPlaneProperty().SetColor(0,0,1)
+                p.GetEdgesProperty().SetColor(0,0,1) #bug in VTK
+            p.GetPlaneProperty().SetOpacity(0.001)
+            #do not draw outline
+            p.GetOutlineProperty().SetColor(0,0,0)
+            p.GetOutlineProperty().SetOpacity(0.0)
+            #do not draw normal
             p.GetSelectedNormalProperty().SetOpacity(0.0)
             p.GetNormalProperty().SetOpacity(0.0)
             p.OutlineTranslationOff()
-            #p.TubingOff()
-            #p.DrawPlaneOff()
+            p.TubingOff()
+            
+            self.cross = vtkPolyData()
+            points = vtkPoints()
+            polys = vtkCellArray()
+            points.SetNumberOfPoints(6)
+            for i in range(3):
+                polys.InsertNextCell(2)
+                polys.InsertCellPoint(2*i); polys.InsertCellPoint(2*i+1)
+            self.cross.SetPoints(points)
+            self.cross.SetLines(polys)
             
             pw = vtkImplicitPlaneWidget2()
             pw.SetRepresentation(p)
@@ -46,18 +71,46 @@ class SlicingPlanesWidget(vtkObject):
         for i in range(3):
             self.planes[i].SetInteractor(interactor)
             self.planes[i].On()
+            
+    def SetRenderer(self, renderer):
+        tubes = vtkTubeFilter()
+        tubes.SetNumberOfSides(16)
+        tubes.SetInput(self.cross)
+        tubes.SetRadius(1.0)
+        
+        crossMapper = vtkPolyDataMapper()
+        crossMapper.SetInput(self.cross)
+        crossActor = vtkActor()
+        crossActor.SetMapper(crossMapper)
+        crossActor.GetProperty().SetColor(0,0,0)
+        renderer.AddActor(crossActor)
     
     def GetCoordinate(self):
         return self.coordinate
         
     def SetCoordinate(self, coor):
+        self.coordinate = coor
         for i in range(3):
             self.planes[i].GetRepresentation().SetOrigin(coor[0], coor[1], coor[2])
+        self.__UpdateCross()
     
+    def __UpdateCross(self):
+        p = self.cross.GetPoints()
+        x,y,z = self.coordinate[0], self.coordinate[1], self.coordinate[2]
+        X,Y,Z = self.dataShape[0], self.dataShape[1], self.dataShape[2] 
+        p.SetPoint(0,  0,y,z)
+        p.SetPoint(1,  X,y,z)
+        p.SetPoint(2,  x,0,z)
+        p.SetPoint(3,  x,Y,z)
+        p.SetPoint(4,  x,y,0)
+        p.SetPoint(5,  x,y,Z)
+        self.cross.Modified()
+        
     def planesCallback(self, obj, event):
         newCoordinate = [self.planes[i].GetRepresentation().GetOrigin()[i] \
                          for i in range(3)]
         if self.coordinate != newCoordinate:
+            self.__UpdateCross()
             self.coordinate = newCoordinate
             self.InvokeEvent("CoordinatesEvent")
         
@@ -114,11 +167,9 @@ class OverviewScene(QWidget):
         self.sceneItems = []
         
         ren = vtkRenderer()
-        ren.SetBackground(0,0,0)
-        ren.SetBackground2(1,1,1)
-        ren.SetGradientBackground(1)
+        ren.SetBackground(1,1,1)
+
         win2 = vtkRenderWindow()
-        #win2 = vtk.vtkGenericOpenGLRenderWindow()
         win2.AddRenderer(ren)
         self.qvtk.SetRenderWindow(win2)
 
@@ -129,10 +180,20 @@ class OverviewScene(QWidget):
 
         self.planes.AddObserver("CoordinatesEvent", self.slicingCallback)
         
+        self.axes = vtkAxesActor();
+        self.axes.AxisLabelsOff()
+        self.axes.SetTotalLength(0.5*shape[0], 0.5*shape[1], 0.5*shape[2])
+        self.axes.SetShaftTypeToCylinder()
+        #transform = vtkTransform()
+        #transform.Translate(-0.125*shape[0], -0.125*shape[1], -0.125*shape[2])
+        #self.axes.SetUserTransform(transform)
+        ren.AddActor(self.axes)
+            
         self.connect(b1, SIGNAL("clicked()"), self.TogglePlaneWidgetX)
         self.connect(b2, SIGNAL("clicked()"), self.TogglePlaneWidgetY)
         self.connect(b3, SIGNAL("clicked()"), self.TogglePlaneWidgetZ)
         
+        self.planes.SetRenderer(ren)
         ren.ResetCamera() 
     
     def display(self, axis):
