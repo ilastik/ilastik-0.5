f9b0c1f425fe00c8b5e86b4b37daf53c78671840
mhanselm <michael.hanselmann@iwr.uni-heidelberg.de>
2011-02-10 18:27:40 +0100

    no more jpgs in tests

--- ilastik/modules/project_gui/core/testModule.py
+++ ilastik/modules/project_gui/core/testModule.py
@@ -18,14 +18,14 @@ class Tests(unittest.TestCase):
     def test_AddFile(self):
         project = projectClass.Project("test", "test", "test")
         
-        fileList = [str(self.testdir + "colorballs.jpg"), str(self.testdir + "zebra.jpg")]
+        fileList = [str(self.testdir + "colorballs.png"), str(self.testdir + "zebra.png")]
         #fileList = [str(self.testdir + "zebra.jpg"), str(self.testdir + "zebra.jpg")]
         project.addFile(fileList)
         print str(self.testdir + "test_two_images.ilp")
-        print str(self.testdir + "syswywy.ilp")
+        print str(self.testdir + "gt_two_images.ilp")
         project.saveToDisk(str(self.testdir + "test_two_images.ilp"))
         same = TestHelperFunctions.compareH5Files(str(self.testdir + "test_two_images.ilp"), 
-                                           str(self.testdir + "syswywy.ilp"))
+                                           str(self.testdir + "gt_two_images.ilp"))
         self.assertEqual(same, True)
         
         
