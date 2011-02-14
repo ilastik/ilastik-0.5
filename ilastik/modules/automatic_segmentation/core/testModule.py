067e7f3ebeb7a96455966d7635863596928d150e
mhanselm <michael.hanselmann@iwr.uni-heidelberg.de>
2011-02-14 16:40:03 +0100

    sorry, forgot to add file#

---
+++ ilastik/modules/automatic_segmentation/core/testModule.py
@@ -0,0 +1,67 @@
+from PyQt4 import QtCore
+import sys
+from ilastik.core.projectClass import Project
+from ilastik.core.testThread import TestThread
+from ilastik.core.testThread import TestHelperFunctions
+from ilastik.modules.automatic_segmentation.core.automaticSegmentationMgr import AutomaticSegmentationModuleMgr
+import unittest
+from ilastik.core import dataImpex
+from ilastik.core import jobMachine
+from ilastik import __path__ as ilastikpath
+
+class TestWholeModule(unittest.TestCase):
+     
+    def setUp(self):
+        #print "setUp"
+        self.testdir = ilastikpath[0] + "/testdata/automatic_segmentation/"
+    
+    def test_WholeModule(self):
+        # create project
+        self.project = Project('Project Name', 'Labeler', 'Description')
+        self.dataMgr = self.project.dataMgr
+    
+        # create file list and load data
+        path = str(self.testdir + "test_image.png")    
+        fileList = []
+        fileList.append(path)
+        self.project.addFile(fileList)
+        
+        # create automatic segmentation manager
+        self.automaticSegmentationMgr = AutomaticSegmentationModuleMgr(self.dataMgr)
+    
+        # setup inputs, compute results
+        inputOverlays = []
+        inputOverlays.append(self.dataMgr[self.dataMgr._activeImageNumber].overlayMgr["Raw Data"])
+        
+        # load precalculated border indicators from file (we use precalculated border indicators since we want to avoid dependencies between the feature module and this one)
+        dataImpex.DataImpex.importOverlay(self.dataMgr[self.dataMgr._activeImageNumber], str(self.testdir + "borders_gaussianGradientMagnitude_sigma0.3_channel2.h5"), "")
+        
+        # overlay lists and filenames
+        listOfResultOverlays = []
+        listOfFilenames = []
+        listOfResultOverlays.append("Auto Segmentation/Segmentation")
+        listOfFilenames.append(self.testdir + "ground_truth_auto_segmentation.h5")
+        
+        # calculate segmentation results
+        # ...import border indicator
+        border_indicator_ov = self.dataMgr[self.dataMgr._activeImageNumber].overlayMgr["border_ind"]
+        input = border_indicator_ov._data[0,:,:,:,0]
+        # ...normalize it
+        input = self.automaticSegmentationMgr.normalizePotential(input)
+        # ...compute results
+        self.automaticSegmentationMgr.computeResults(input)
+        # ...add overlays
+        self.automaticSegmentationMgr.finalizeResults()
+        
+        # compare obtained result to ground truth result
+        equalOverlays = TestHelperFunctions.compareResultsWithFile(self.automaticSegmentationMgr, listOfResultOverlays, listOfFilenames)
+        self.assertEqual(equalOverlays, True)
+        
+    def tearDown(self):
+        jobMachine.GLOBAL_WM.stopWorkers()        
+
+if __name__ == "__main__":
+    unittest.main()
+    jobMachine.GLOBAL_WM.stopWorkers()
+
+
