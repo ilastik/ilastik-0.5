22984e6a2b5bdb7ee895b502e3068d85c8322c81
Thorben Kroeger <thorben.kroeger@iwr.uni-heidelberg.de>
2011-02-22 14:57:21 +0100

    improve script

--- scripts/coverage.sh
+++ scripts/coverage.sh
@@ -1,3 +1,8 @@
-nosetests  --with-coverage --cover-package=ilastik
-python-coverage html -d coverage
+find . -name \*.pyc | xargs rm
+nosetests  --with-coverage --cover-erase 
+if [ ! -d /etc/portage ]; then
+    python-coverage html -d coverage
+else
+    coverage html -i -d coverage
+fi
 
