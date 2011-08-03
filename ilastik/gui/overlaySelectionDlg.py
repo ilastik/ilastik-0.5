3760f3aa8090eb4f26b175a1fbaa1b69d12a3412
opetra <oliver.petra@iwr.uni-heidelberg.de>
2011-08-03 13:47:59 +0200

    adjusted layout

--- ilastik/gui/overlaySelectionDlg.py
+++ ilastik/gui/overlaySelectionDlg.py
@@ -12,11 +12,19 @@ class OverlaySelectionDialog(QDialog):
         self.layout = QHBoxLayout()
         self.setLayout(self.layout)
         
+        self.treeGroupBox = QGroupBox("  Overlays")
+        self.treeGroupBox.setFlat(True)
+        self.treeGroupBox.setStyleSheet("border:0;")
         self.overlayTreeWidgetLayout = QHBoxLayout()
         self.overlayTreeWidget = treeWidget.OverlayTreeWidget()
         self.overlayTreeWidgetLayout.addWidget(self.overlayTreeWidget)
         self.overlayTreeWidget.itemSelectionChanged.connect(self.overlayTreeItemSelectionChanged)
+        self.treeGroupBox.setLayout(self.overlayTreeWidgetLayout)
         
+        self.preViewBoxAndButtonLayout = QVBoxLayout()
+        self.preViewAndSpinBoxGroupBox = QGroupBox("  Preview")
+        self.preViewAndSpinBoxGroupBox.setFlat(True)
+        self.preViewAndSpinBoxGroupBox.setStyleSheet("border:0;")
         self.preViewAndSpinBoxLayout = QVBoxLayout()
         self.preView = preView.PreView()
         self.preViewAndSpinBoxLayout.addWidget(self.preView)
@@ -39,24 +47,34 @@ class OverlaySelectionDialog(QDialog):
 #        self.spinBoxLayout.addStretch()
         
         self.preViewAndSpinBoxLayout.addLayout(self.spinBoxLayout)
+        self.preViewAndSpinBoxGroupBox.setLayout(self.preViewAndSpinBoxLayout)
+        self.preViewBoxAndButtonLayout.addWidget(self.preViewAndSpinBoxGroupBox)
                 
         self.buttonLayout = QHBoxLayout()
+        self.buttonLayout.setSpacing(4)
         self.add_selectedButoon = QToolButton()
         self.add_selectedButoon.setText("Add Selected")
-        self.add_selectedButoon.clicked.connect(self.on_add_selectedClicked)
+        self.add_selectedButoon.clicked.connect(self._on_add_selectedClicked)
         self.cancelButton = QToolButton()
         self.cancelButton.setText("Cancel")
-        self.cancelButton.clicked.connect(self.on_cancelClicked)
+        self.cancelButton.clicked.connect(self._on_cancelClicked)
         self.buttonLayout.addStretch()
         self.buttonLayout.addWidget(self.add_selectedButoon)
         self.buttonLayout.addWidget(self.cancelButton)
         self.buttonLayout.addSpacerItem(QSpacerItem(10,0))
-        self.preViewAndSpinBoxLayout.addLayout(self.buttonLayout)
-        self.preViewAndSpinBoxLayout.addSpacerItem(QSpacerItem(0,10))
+        self.preViewBoxAndButtonLayout.addSpacerItem(QSpacerItem(10,10))
+        self.preViewBoxAndButtonLayout.addLayout(self.buttonLayout)
+        self.preViewBoxAndButtonLayout.addSpacerItem(QSpacerItem(0,10))
         
-        self.layout.addLayout(self.overlayTreeWidgetLayout)
-        self.layout.addLayout(self.preViewAndSpinBoxLayout)
+        self.layout.addWidget(self.treeGroupBox)
+        self.layout.addLayout(self.preViewBoxAndButtonLayout)
+        self.overlayTreeWidgetLayout.setContentsMargins(0, 10, 0, 0)
+        self.treeGroupBox.setContentsMargins(4, 10, 4, 4)
+        self.preViewAndSpinBoxLayout.setContentsMargins(0, 10, 0, 0)
+        self.preViewAndSpinBoxGroupBox.setContentsMargins(4, 10, 4, 4)
+        self.preViewBoxAndButtonLayout.setContentsMargins(0, 0, 0, 0)
         self.layout.setContentsMargins(0,0,0,0)
+        self.layout.setSpacing(0)
         
     # methods
     # ------------------------------------------------
@@ -77,12 +95,17 @@ class OverlaySelectionDialog(QDialog):
     def createOverlayTreeWidget(self, overlayDict, forbiddenOverlays, preSelectedOverlays):
         self.overlayTreeWidget.addOverlaysToTreeWidget(overlayDict, forbiddenOverlays, preSelectedOverlays)
         
-    def on_add_selectedClicked(self):
+    def _on_add_selectedClicked(self):
         self.accept()
     
-    def on_cancelClicked(self):
+    def _on_cancelClicked(self):
         self.reject()
     
+    def exec_(self):
+        if QDialog.exec_(self) == QDialog.Accepted:
+            return self.overlayTreeWidget.createSelectedItemList()  
+        else:
+            return []
     
     
     
@@ -99,6 +122,12 @@ if __name__ == "__main__":
     from PyQt4.QtGui import *
         
     app = QApplication(sys.argv)
+    
+#    app.setStyle("windows")
+#    app.setStyle("motif")
+#    app.setStyle("cde")
+#    app.setStyle("plastique")
+#    app.setStyle("macintosh")
     app.setStyle("cleanlooks")
     
     ex1 = OverlaySelectionDialog()
