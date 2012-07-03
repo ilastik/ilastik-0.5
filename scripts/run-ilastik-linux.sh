ae6682e9e1fdacb0173d811a00cf3e01742d61ee
Markus Nullmeier <mi4h5hi9de25phs1@arcor.de>
2012-07-03 19:26:59 +0200

    fix startup script

--- scripts/run-ilastik-linux.sh
+++ scripts/run-ilastik-linux.sh
@@ -1,7 +1,8 @@
-PREFIX="."
+#!/bin/bash
+ILASTIK_SCRIPT=$(readlink -f $0)
+PREFIX=$(dirname $ILASTIK_SCRIPT)
 
 export LD_LIBRARY_PATH=$PREFIX/lib
 export PATH=$PREFIX/bin:$PATH
-export PYTHONPATH=.:$PREFIX/lib:$PREFX/lib/python2.7/site-packages
-$PREFIX/bin/python2.7 ilastik/ilastikMain.py
-
+export PYTHONPATH=$PREFIX:$PREFIX/lib:$PREFX/lib/python2.7/site-packages
+$PREFIX/bin/python2.7 $PREFIX/ilastik/ilastikMain.py
